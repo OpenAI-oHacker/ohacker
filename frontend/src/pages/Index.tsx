31492b42fd3cfa60d19e781075697943c13f96cc
Michał Pstrąg <michal.pstrag@icloud.com>
2025-04-26 01:43:48 +0200

    sync comment after submit

--- frontend/src/pages/Index.tsx
+++ frontend/src/pages/Index.tsx
@@ -27,18 +27,21 @@ const Index: React.FC = () => {
       try {
         const images_response = await fetch('http://127.0.0.1:8001/images');
         const images_data = await images_response.json();
-
-        const comments_promises = images_data.images.map(image => 
-          fetch(`http://127.0.0.1:8001/comments/${image}`)
-            .then(response => response.json())
-            .then(response => response.comments.map(comment => ({
-              id: Date.now(),
-              user: getRandomUser(),
-              text: comment.comment_text,
-            })))
-        );
-        const comments_data = await Promise.all(comments_promises);
-        console.log(comments_data)
+        let comments_data = [];
+        try {
+          const comments_promises = images_data.images.map(image => 
+            fetch(`http://127.0.0.1:8001/comments/${image}`)
+              .then(response => response.json())
+              .then(response => response.comments.map((comment, index) => ({
+                id: `${image}-${index}`,
+                user: getRandomUser(),
+                text: comment.comment_text,
+              })))
+          );
+          comments_data = await Promise.all(comments_promises);
+        } catch (err) {
+          console.error(err);
+        }
         setPosts(images_data.images.map((image, index) => ({
           id: image,
           user: getRandomUser(),
@@ -66,7 +69,7 @@ const Index: React.FC = () => {
   };
 
   const handleAddComment = async (postId: number, text: string) => {
-    const response = await fetch(`http://127.0.0.1:8001/comments/${postId}`, {
+    await fetch(`http://127.0.0.1:8001/comments/${postId}`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
@@ -75,19 +78,28 @@ const Index: React.FC = () => {
         comment_text: text,
       })
     })
+
+    const response = await fetch(`http://127.0.0.1:8001/comments/${postId}`)
+    const data = await response.json()
+    const serverComments = data.comments.map((comment, index) => ({
+      id: `${postId}-${index}`,
+      user: MOCK_USER,
+      text: comment.comment_text,
+    }))
+
     setPosts((prev) =>
       prev.map((p) =>
         p.id === postId
           ? {
               ...p,
-              comments: [
-                ...p.comments,
-                {
-                  id: Date.now(),
-                  user: getRandomUser(),
-                  text,
-                },
-              ],
+              comments: serverComments.map((comment, index) => {
+                const existingComment = p.comments.find(ec => ec.text === comment.text);
+                return {
+                  id: `${postId}-${index}`,
+                  user: existingComment ? existingComment.user : MOCK_USER,
+                  text: comment.text,
+                }
+              }),
             }
           : p
       )
